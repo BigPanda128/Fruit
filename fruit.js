ecf99db0faeafaeacef3cebcb383fe614261f780
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-02-16 12:15:30 -0500

    fruit code in JSON

---
+++ fruit.js
@@ -0,0 +1,13 @@
+//Ryan Postma
+//CMP237
+//2/16/2015
+//fruit mock exam test code
+
+
+var fruit = [ { "name": "apple", "color": "red", "quantity": 3 }, 
+{ "name": "pear", "color": "green", "quantity": 2 },
+{ "name": "grape", "color": "purple", "quantity": 15 },
+{ "name": "tomato", "color": "red", "quantity": 5 },
+{ "name": "watermelon", "color": "green", "quantity": 1 }];
+
+print(fruit);
